977ebe92936959b0bb3c90296e5ffb056c8a843f
Thalya Teixeira <thalyateixeira9@hotmail.com>
2022-08-30 09:57:07 -0300

    Iniciando projeto

---
+++ ProjBancoMorangao/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjBancoMorangao
+{
+    internal class Endereco
+    {
+        protected string Logradouro { get; set; }
+        protected string Numero { get; set; }
+        protected string Bairro { get; set; }
+        protected string Cep { get; set; }
+        protected string Cidade { get; set; }
+        protected string Estado { get; set; }
+
+
+    }
+}
