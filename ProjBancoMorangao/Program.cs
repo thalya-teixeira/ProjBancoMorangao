7810444c92bd278527869fe43f3fd5c18cade583
Thalya Teixeira <thalyateixeira9@hotmail.com>
2022-08-30 09:39:23 -0300

    Adicionar arquivos de projeto.

---
+++ ProjBancoMorangao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjBancoMorangao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
